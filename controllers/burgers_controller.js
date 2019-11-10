bfff7ab657162f4bdac3a6813f63d3fe4f1c323e
Bishop Bader <bflatbader@users.noreply.github.com>
2019-11-10 18:32:45 -0500

    Got burger data to display on handlebars layout when server is run

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,9 +1,19 @@
+// DEPENDENCIES
 var express = require("express");
-
 var router = express.Router();
-
-// Import the model to use its database functions.
 var burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+    console.log("Getting burger data...")
+    burger.all(function(data) {
+        var burgersData = {
+        burgers: data
+    };
+    console.log(burgersData);
+    res.render("index", burgersData);
+    });
+});
 
 // Export routes for server.js to use.
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // DEPENDENCIES
 var express = require("express");
+var exphbs = require("express-handlebars");
+var routes = require("./controllers/burgers_controller.js");
 
 var PORT = process.env.PORT || 8080;
 
@@ -12,14 +14,9 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
-// Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
