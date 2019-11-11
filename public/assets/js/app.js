981f5f9592fb31a8f8f0e6d6db8c5ed4ad090396
Bishop Bader <bflatbader@users.noreply.github.com>
2019-11-10 22:21:13 -0500

    Added functionality to unorder, added new buttons for Delete functionality to be added later

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,25 +1,4 @@
 $(function() {
-    // Shopping bag button is clicked to order burger
-    $(".change-devour").on("click", function(event) {
-        var id = $(this).data("id");
-            
-        var newDevouredState = {
-          devoured: true
-        };
-    
-        // Send the PUT request.
-        $.ajax("/api/burgers/" + id, {
-          type: "PUT",
-          data: newDevouredState
-        }).then(
-          function() {
-            console.log("ordered burger " + id);
-            // Reload the page to get the updated list
-            location.reload();
-          }
-        );
-      });
-
     // Plus button is clicked to add a new burger
     $("#submit").click(function(event) {
         // Make sure to preventDefault on a submit event.
@@ -41,4 +20,45 @@ $(function() {
           }
         );
     });    
+
+    // Shopping bag button is clicked to order burger
+    $(".change-devour").on("click", function(event) {
+        var id = $(this).data("id");
+            
+        var newDevouredState = {
+          devoured: true
+        };
+    
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+          type: "PUT",
+          data: newDevouredState
+        }).then(
+          function() {
+            console.log("ordered burger " + id);
+            // Reload the page to get the updated list
+            location.reload();
+          }
+        );
+    });
+
+    $(".un-devour").on("click", function(event) {
+        var id = $(this).data("id");
+            
+        var newDevouredState = {
+          devoured: false
+        };
+    
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+          type: "PUT",
+          data: newDevouredState
+        }).then(
+          function() {
+            console.log("removed burger " + id);
+            // Reload the page to get the updated list
+            location.reload();
+          }
+        );
+    });
 });
