204e97c56195a90732c0d6181969a0d106f6db28
Bishop Bader <bflatbader@users.noreply.github.com>
2019-11-11 20:44:12 -0500

    adding db connection logging

--- config/connection.js
+++ config/connection.js
@@ -16,6 +16,13 @@ if (process.env.JAWSDB_URL) {
 };
 
 // Establish connection
-connection.connect();
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
 // Export connection for our ORM to use.
 module.exports = connection;
