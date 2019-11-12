e2ab061580441b923d5c313318e3f6e3fe0b2075
Bishop Bader <bflatbader@users.noreply.github.com>
2019-11-11 20:36:35 -0500

    Added JawsDB connection information

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,21 @@
 // DEPENDENCIES
 var mysql = require("mysql");
+var connection;
 
-// Configure mysql connection
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-});
+// Configure mysql connection for JAWSDB or local
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: "password",
+      database: "burgers_db"
+  });
+};
 
 // Establish connection
-connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
-
+connection.connect();
 // Export connection for our ORM to use.
 module.exports = connection;
